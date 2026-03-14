b15e13ef2d6b0395bda617eff509d30b6ae2b035
Ting Wang <wangting1701@163.com>
2026-03-14 22:20:52 +0800

    feat: api init

---
+++ SkillSnap.Api/Models/Project.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SkillSnap.Api.Models;
+
+public class Project
+{
+    [Key]
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+
+    [ForeignKey("PortfolioUserId")]
+    public int PortfolioUserId { get; set; }
+}
---
+++ SkillSnap.Api/Program.cs
@@ -0,0 +1,42 @@
+using SkillSnap.Client.Pages;
+using SkillSnap.Api.Components;
+using SkillSnap.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddDbContext<SkillSnapContext>(options =>
+    options.UseSqlite("Data Source=skillsnap.db"));
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(SkillSnap.Client._Imports).Assembly);
+
+app.Run();
